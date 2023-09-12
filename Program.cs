b597e9269f89ed1ac67e8b4f318dce42957bbd64
Aspinall <Paul.Aspinall@landg.com>
2023-09-12 11:43:01 +0100

    Added a simple multiplier

--- Program.cs
+++ Program.cs
@@ -4,5 +4,11 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Welcome to my calculator!");
+        Console.Write("Enter your first number: ");
+        double firstNumber = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Enter your second number: ");
+        double secondNumber = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("The result is: {0}", firstNumber * secondNumber);
+
     }
 }
