299d24cce2fd1194a57e3278e855c7f0a36dc97d
Aspinall <Paul.Aspinall@landg.com>
2023-09-13 11:32:10 +0100

    Added operator choice

--- Program.cs
+++ Program.cs
@@ -3,12 +3,36 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Welcome to my calculator!");
-        Console.Write("Enter your first number: ");
+        Console.WriteLine("Welcome to the calculator!");
+        Console.WriteLine("==========================");
+        Console.Write("Please enter the operator (+, -, /, *): ");
+        string choice = Console.ReadLine()!;
+        Console.Write("Please enter the first number: ");
         double firstNumber = Convert.ToDouble(Console.ReadLine());
-        Console.Write("Enter your second number: ");
+        Console.Write("Please enter the second number: ");
         double secondNumber = Convert.ToDouble(Console.ReadLine());
-        Console.WriteLine("The result is: {0}", firstNumber * secondNumber);
+
+        double result = 0;
+
+        switch (choice)
+        {
+            case "+":
+                result = firstNumber + secondNumber;
+                break;
+            case "-":
+                result = firstNumber - secondNumber;
+                break;
+            case "*": 
+                result = firstNumber * secondNumber;
+                break;
+            case "/":
+                result = firstNumber / secondNumber;
+                break;
+            default:
+                Console.WriteLine("This is not a valid operator");
+                break;
+        }
+        Console.WriteLine("The answer is: {0}", result);
 
     }
 }
