c9cdb9d35dff121bc0fb600a999fedf4423c6bc8
Aspinall <Paul.Aspinall@landg.com>
2023-09-15 12:27:03 +0100

    Completed exercise 5.5

--- Program.cs
+++ Program.cs
@@ -3,19 +3,55 @@ class Program
 {
     static void Main(string[] args)
     {
+        PrintWelcomeMessage();
+        PerformOneCalculation();       
+    }
+    private static void PrintWelcomeMessage()
+    {
         Console.WriteLine("Welcome to the calculator!");
         Console.WriteLine("==========================");
-        Console.Write("Please enter the operator (+, -, /, *): ");
-        string operatorChoice = Console.ReadLine()!;
-        Console.Write("How many numbers do you want to {0}?: ", operatorChoice);
-        int numberChoice = Convert.ToInt32(Console.ReadLine());
+    }
+    private static void PerformOneCalculation()
+    {
+        bool condition = true;   
+        do
+        {
+            Console.Write("Please enter the operator (+, -, /, *) or return to exit: ");
+            string operatorChoice = Console.ReadLine()!;
+        
+            if (operatorChoice == string.Empty)
+            {
+                condition = false;
+            }
+            else
+            {
+                Console.Write("How many numbers do you want to {0}?: ", operatorChoice);
+                int numberChoice = Convert.ToInt32(Console.ReadLine());
 
+                InputNumbers(operatorChoice, numberChoice);
+            }
+        
+        } while (condition);
+    } 
+    private static void InputNumbers(string operatorChoice, int numberChoice)
+    {
         double result = 0;
+        
         for (int i = 1; i <= numberChoice; i++)
         {
             Console.Write("Please enter number {0}: ", i);
-            double number = Convert.ToDouble(Console.ReadLine()); 
-            if (i == 1)
+            //double number = Convert.ToDouble(Console.ReadLine()); 
+            string numString = Console.ReadLine()!;
+            
+            if (int.TryParse(numString, out int number))
+            {
+                
+            } else
+            {
+                Console.WriteLine("That wasn't a valid number, please try again.");
+                i--;
+            }
+            if (numberChoice == 1)
             {
                 result = number;
             }  
@@ -40,9 +76,9 @@ class Program
                         break;
                 }
             }
+            
         }
         
         Console.WriteLine("The answer is: {0}", result);
-
     }
 }
